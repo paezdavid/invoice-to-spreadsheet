a842320cde5460cd316d6be01780ebd1e10608e2
paezdavid <paezdavidr@gmail.com>
2025-06-30 23:08:38 -0300

    improve formatting and cell structure; set names in English

--- script.py
+++ script.py
@@ -5,9 +5,10 @@ workbook = xlsxwriter.Workbook('../finanzas.xlsx')
 worksheet = workbook.add_worksheet()
 
 # formatting
-cell_styling = workbook.add_format({'align': 'center', 'valign': 'vcenter'})
+cell_styling = workbook.add_format({'align': 'center', 'valign': 'vcenter', 'text_wrap': True})
 currency_styling = workbook.add_format({'align': 'center', 'valign': 'vcenter', 'num_format': '#,##0.00'})
 
+# headers
 worksheet.write('A1', 'CDC')
 worksheet.write('B1', 'FECHA EMISION')
 worksheet.write('C1', 'PRODUCTO/SERVICIO')
@@ -19,48 +20,52 @@ worksheet.write('H1', 'TOTAL PAGADO')
 worksheet.write('I1', 'NOMBRE O RAZON SOCIAL')
 worksheet.write('J1', 'COMERCIO')
 
-ticket_row = 0
-product_row = 0
-for file_path in ['../file1.xml', '../file2.xml']:
+# width
+worksheet.set_column(0, 0, 10)
+worksheet.set_column(2, 2, 50)
+
+ticket_row = 1
+product_row = 1
+for file_path in ['../file2.xml', '../file1.xml', '../file.xml']:
     tree = objectify.parse(file_path)
     root = tree.getroot()
 
-    # Campos
+    # Fields
     cdc = root.DE.attrib['Id']  # Codigo de Control
     fecha_emision_DE = root.DE.gDatGralOpe.dFeEmiDE.text  # DE -> Documento Electronico
-    lista_de_productos = [{
-                            "nombre": p.dDesProSer.text,
-                            "cantidad": p.dCantProSer.text,
-                            "unidad_de_medida": p.dDesUniMed.text,
-                            "precio_unitario": p.gValorItem.dPUniProSer,
-                            "precio_bruto": p.gValorItem.dTotBruOpeItem
+    product_services_list = [{
+                            "name": p.dDesProSer.text,
+                            "quantity": round(float(p.dCantProSer.text), 3),
+                            "unit_of_measure": p.dDesUniMed.text,
+                            "unit_price": p.gValorItem.dPUniProSer,
+                            "gross_total": p.gValorItem.dTotBruOpeItem
                             } \
                           for p in root.DE.gDtipDE.gCamItem]
-    total_pagado = root.DE.gTotSub.dTotOpe.text
-    nombre_razon_social = root.DE.gDatGralOpe.gDatRec.dNomRec.text
-    comercio = root.DE.gDatGralOpe.gEmis.dNomEmi.text
+    amount_paid_total = float(root.DE.gTotSub.dTotOpe.text)
+    company_name = root.DE.gDatGralOpe.gDatRec.dNomRec.text  # nombre o razon social
+    seller = root.DE.gDatGralOpe.gEmis.dNomEmi.text
 
-    for producto in lista_de_productos:
-        worksheet.write(product_row + 1, 2, producto['nombre'])
-        worksheet.write(product_row + 1, 3, producto['cantidad'])
-        worksheet.write(product_row + 1, 4, producto['unidad_de_medida'])
-        worksheet.write(product_row + 1, 5, producto['precio_unitario'])
-        worksheet.write(product_row + 1, 6, producto['precio_bruto'])
+    for product_service in product_services_list:
+        worksheet.write(product_row, 2, product_service['name'], cell_styling)
+        worksheet.write(product_row, 3, product_service['quantity'], currency_styling)
+        worksheet.write(product_row, 4, product_service['unit_of_measure'], cell_styling)
+        worksheet.write(product_row, 5, product_service['unit_price'], currency_styling)
+        worksheet.write(product_row, 6, product_service['gross_total'], currency_styling)
         product_row += 1
 
-    if len(lista_de_productos) > 1:
-        worksheet.merge_range(ticket_row + 1, 0, ticket_row + len(lista_de_productos), 0, cdc, cell_styling)
-        worksheet.merge_range(ticket_row + 1, 1, ticket_row + len(lista_de_productos), 1, fecha_emision_DE, cell_styling)
-        worksheet.merge_range(ticket_row + 1, 7, ticket_row + len(lista_de_productos), 7, total_pagado)
-        worksheet.merge_range(ticket_row + 1, 8, ticket_row + len(lista_de_productos), 8, nombre_razon_social, cell_styling)
-        worksheet.merge_range(ticket_row + 1, 9, ticket_row + len(lista_de_productos), 9, comercio, cell_styling)
+    if len(product_services_list) > 1:
+        worksheet.merge_range(ticket_row, 0, ticket_row + len(product_services_list) - 1, 0, cdc, cell_styling)
+        worksheet.merge_range(ticket_row, 1, ticket_row + len(product_services_list) - 1, 1, fecha_emision_DE, cell_styling)
+        worksheet.merge_range(ticket_row, 7, ticket_row + len(product_services_list) - 1, 7, amount_paid_total, currency_styling)
+        worksheet.merge_range(ticket_row, 8, ticket_row + len(product_services_list) - 1, 8, company_name, cell_styling)
+        worksheet.merge_range(ticket_row, 9, ticket_row + len(product_services_list) - 1, 9, seller, cell_styling)
     else:
-        worksheet.write(ticket_row + 1, 0, cdc)
-        worksheet.write(ticket_row + 1, 1, fecha_emision_DE)
-        worksheet.write(ticket_row + 1, 7, total_pagado)
-        worksheet.write(ticket_row + 1, 8, nombre_razon_social)
-        worksheet.write(ticket_row + 1, 9, comercio)
-    ticket_row = len(lista_de_productos)
+        worksheet.write(ticket_row, 0, cdc, cell_styling)
+        worksheet.write(ticket_row, 1, fecha_emision_DE, cell_styling)
+        worksheet.write(ticket_row, 7, amount_paid_total, currency_styling)
+        worksheet.write(ticket_row, 8, company_name, cell_styling)
+        worksheet.write(ticket_row, 9, seller, cell_styling)
+    ticket_row = product_row
 
 
 workbook.close()
